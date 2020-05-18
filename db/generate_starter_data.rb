d62467efa1451eb04967e8e84cf093239112d882
thenora <thenora@gmail.com>
2020-05-17 17:04:47 -0700

    added pizza csv and updated seed related files

--- db/generate_starter_data.rb
+++ db/generate_starter_data.rb
@@ -10,15 +10,15 @@ require "csv"
 # $ rails db:reset
 # doesn't currently check for if titles are unique against each other
 
-CSV.open("db/works_seeds.csv", "w", :write_headers => true,
-                                    :headers => ["category", "title", "creator", "publication_year", "description"]) do |csv|
+CSV.open("db/pizzas-seeds.csv", "w", :write_headers => true,
+                                    :headers => ["crust", "name", "sauce", "cheese", "toppings"]) do |csv|
   25.times do
-    category = %w(album book).sample
-    title = Faker::Coffee.blend_name
-    creator = Faker::Name.name
-    publication_year = rand(Date.today.year - 100..Date.today.year)
-    description = Faker::Lorem.sentence
+    crust = %w(thin thick deep-dish).sample
+    name = Faker::Food.dish
+    sauce = %w(Red White Korean-BBQ Pesto Tikka-Masala EVOO).sample
+    cheese = %w(Mozzarella Cheddar Cashew Parmesan).sample
+    toppings = Faker::Food.ingredient
 
-    csv << [category, title, creator, publication_year, description]
+    csv << [crust, name, sauce, cheese, toppings]
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+PIZZA_MENU = Rails.root.join('db', 'pizzas-seeds.csv')
+
+pizza_fails = []
+CSV.foreach(PIZZA_MENU, :headers => true) do |row|
+  pizza = Pizza.new
+  pizza.crust = row['crust']
+  pizza.name = row['name']
+  pizza.sauce = row['sauce']
+  pizza.cheese = row['cheese']
+  pizza.toppings = row['toppings']
+  successful = pizza.save
+  if !successful
+    pizza_fails << pizza
+    puts "Failed to save pizza: #{pizza.inspect}"
+  else
+    puts "Created pizza: #{pizza.inspect}"
+  end
+end
+
+puts "Added #{Pizza.count} pizza records."
+puts "#{pizza_fails.length} pizzas failed to save."
