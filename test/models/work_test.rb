a9beb116f3735199324422b3d8196b922b512f30
thenora <thenora@gmail.com>
2020-05-20 16:47:54 -0700

    added more validation tests and fixed unique title

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -13,6 +13,8 @@ describe Work do
     before do
       # Arrange
       @movie1 = works(:movie1)
+      @movie2 = works(:movie2)
+      @movie3 = works(:movie3)
       @book1 = works(:book1)
     end
 
@@ -36,21 +38,75 @@ describe Work do
       expect(@movie1.errors.messages).must_include :title
     end
 
-    # TODO Add more is invalid without validation tests
-    # TODO add publication date must be number
+    it 'is invalid without a category' do
+      # Arrange
+      @movie1.category = nil
+    
+      # Act
+      result = @movie1.valid?
+    
+      # Assert
+      expect(result).must_equal false
+      expect(@movie1.errors.messages).must_include :category
+    end
 
-    # TODO fix unique title test
+    it 'is invalid without a creator' do
+      # Arrange
+      @movie1.creator = nil
+    
+      # Act
+      result = @movie1.valid?
+    
+      # Assert
+      expect(result).must_equal false
+      expect(@movie1.errors.messages).must_include :creator
+    end
+
+    it 'is invalid without a description' do
+      # Arrange
+      @movie1.description = nil
+    
+      # Act
+      result = @movie1.valid?
+    
+      # Assert
+      expect(result).must_equal false
+      expect(@movie1.errors.messages).must_include :description
+    end
+
+    it 'is invalid without a publication_year' do
+      # Arrange
+      @movie1.publication_year = nil
+    
+      # Act
+      result = @movie1.valid?
+    
+      # Assert
+      expect(result).must_equal false
+      expect(@movie1.errors.messages).must_include :publication_year
+    end
+
+    it 'is invalid if publication year is not a number' do
+      # Arrange
+      @movie2.publication_year = 'Tuesday'
+    
+      # Act
+      result = @movie2.valid?
+    
+      # Assert
+      expect(result).must_equal false
+      expect(@movie2.errors.messages).must_include :publication_year
+    end
+
     it 'is invalid with a non-unique title' do
       # Arrange
-       dup_book = Work.create(
+       @dup_book = Work.create(
         title: 'book1', 
         category: 'book',
         creator: 'Me',
         description: 'Blah blah stuff n things.',
         publication_year: 2011
         )
-
-        # unique_book.save
 
       # Act
 
