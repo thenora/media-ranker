5d1ecc7a258a2a7a4e9ed0ac89694573834973b0
thenora <thenora@gmail.com>
2020-05-22 00:02:45 -0700

    add votes relationship to works

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_065602) do
+ActiveRecord::Schema.define(version: 2020_05_22_070117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2020_05_22_065602) do
   create_table "votes", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "work_id"
+    t.index ["work_id"], name: "index_votes_on_work_id"
   end
 
   create_table "works", force: :cascade do |t|
@@ -36,4 +38,5 @@ ActiveRecord::Schema.define(version: 2020_05_22_065602) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "votes", "works"
 end
