bac2b9e6b121d2c943369279c657d3689e69949b
thenora <thenora@gmail.com>
2020-05-22 00:24:26 -0700

    added require login to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  def current_user
+    return @current_user = User.find_by(id: session[:user_id])
+  end
+
+  def require_login
+    if @current_user.nil?
+      flash[:error] = "You must log in to do that"
+      redirect_to login_path # TODO Do I need this?
+      return
+    end
+  end
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,32 @@
 class VotesController < ApplicationController
+  before_action :require_login, only: [:create]
+
+  def create
+    user = User.find_by(id: session[:user_id])
+    work = Work.find_by(id: params[:work_id])
+    
+    if user.nil? || work.nil?
+      head :not_found
+      return
+    end
+    
+    if session[:user_id]
+      vote = Vote.create(user_id: user.id, work_id: work.id)
+      if vote.save 
+        flash[:success] = 'Successfully upvoted!'
+      else
+        flash[:error] = 'You already voted for this'
+      end
+    end
+    
+    redirect_to request.referrer || work_path(work.id)
+    return
+  end
+  
+  
+  private
+  
+  def votes_params
+    params.require(:votes).permit(:user_id, :work_id, :created_at)
+  end
 end
