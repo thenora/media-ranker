5ec0a7050112d50dc4020ff33c7434c4ce641ae9
thenora <thenora@gmail.com>
2020-05-23 01:29:19 -0700

    add foreign key schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_070117) do
+ActiveRecord::Schema.define(version: 2020_05_23_082750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_05_22_070117) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "vote_id"
+    t.index ["vote_id"], name: "index_users_on_vote_id"
   end
 
   create_table "votes", force: :cascade do |t|
@@ -38,5 +40,6 @@ ActiveRecord::Schema.define(version: 2020_05_22_070117) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "users", "votes"
   add_foreign_key "votes", "works"
 end
