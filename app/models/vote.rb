2d0038d64d1d9d4f58c78acb0eb325164880c1db
thenora <thenora@gmail.com>
2020-05-22 22:18:29 -0700

    added belongs to and validation to vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,2 +1,8 @@
 class Vote < ApplicationRecord
+  belongs_to :user
+  belongs_to :work
+  
+  validates :user_id, presence: true
+  validates :work_id, presence: true, uniqueness: { scope: :user_id }
+
 end
