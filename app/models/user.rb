02f481493b9b91dd8467602e6ade9c568970eb77
thenora <thenora@gmail.com>
2020-05-24 15:07:20 -0700

    added dependent destroy to has many votes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :votes
+  has_many :votes, dependent: :destroy
 	has_many :works, through: :votes
 
 	validates :username, presence: true, uniqueness: true
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,5 @@
 class Work < ApplicationRecord
-  has_many :votes
+  has_many :votes, dependent: :destroy
   has_many :users, :through => :votes
 
   validates :category, :title, :creator, :description, presence: true
